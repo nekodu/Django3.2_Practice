505ddb08fba4cbd4fed283ffb081df5a10e3c92b
nekodu <kaplangokce@yandex.com>
2022-06-08 17:18:31 +0300

    CommitV2

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,16 +1,18 @@
 """ 
 To render html pages
 """
-
+import random
 from django.http import HttpResponse
 
-HTML_STRING = """
-<h1> Hello World</h1>
-"""
 
-def home_view():
+
+def home_view(request):
     """
     Take in a request(Django does that)
     Return html as a response (we do that)
+    """
+    number = random.randint(1,99)
+    HTML_STRING = f"""
+    <h1> Hello User {number}!</h1>
     """
     return HttpResponse(HTML_STRING)
